c8068dffe1ba371b655dd957d539d29400aa55dd
tungnt90 <tungnt90@gmail.com>
2023-02-15 16:43:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,10 +36,7 @@ setuid 65535
 flush
 auth none
 
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
+$(awk -F "/" '{print "auth none\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
